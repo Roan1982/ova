22019f221bbd6b50fc7444f48a4975e4f70b6b16
Roan82 <127224094+Roan1982@users.noreply.github.com>
2025-10-29 08:23:57 -0300

    Change database configuration to PostgreSQL
    Updated database settings to use PostgreSQL instead of SQLite.

--- emergency_system/emergency_app/settings.py
+++ emergency_system/emergency_app/settings.py
@@ -87,16 +87,13 @@ WSGI_APPLICATION = 'emergency_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
-# Si más adelante querés usar PostgreSQL en Render, descomentá este bloque y
-# comentá el anterior.
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -107,7 +104,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
